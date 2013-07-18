4c39e0546152f12b6b2ea0411c191e61a5d8a4bd
Prasenjit Saha <psaha@physik.uzh.ch>
2013-07-18 14:16:22 +0200

    kappa functions for checking

--- systems/kappas.py
+++ systems/kappas.py
@@ -1,12 +1,22 @@
-from numpy import pi,cos,arctan2,arctan,arctanh,sqrt
+from numpy import sqrt,pi,cos,arctan2,arctan,arctanh
+
+def kappa_SIE(x,y,reinst,ell,ell_pa):
+    pa = ell_pa*pi/180 + pi/2
+    phi = arctan2(y,x) - pa
+    r = sqrt( (x*x + y*y + 1e-6) * (1 - ell*cos(2*phi)) )
+    return reinst/r
+    
+def poten_shear(x,y,sh_str,sh_pa):
+    thg = sh_pa*pi/180 + pi/2
+    rsq = x*x + y*y
+    f = 2*(arctan2(y,x) - thg)
+    return -sh_str/2*rsq*cos(f)
 
 def kappa_NFW(x,y,gx,gy,kappa,rscale,ell,ell_pa):
     pa = ell_pa*180/pi + pi/2
     x,y = x-gx,y-gy
     phi = arctan2(y,x) - pa
-    th2r = x*x + y*y + 1e-6
-    th2p = 1 - ell*cos(2*phi)
-    r = sqrt(th2r*th2p)
+    r = sqrt( (x*x + y*y + 1e-6) * (1 - ell*cos(2*phi)) )
     u = r/rscale
     if u==1.:
         f = 1./3
@@ -16,6 +26,6 @@ def kappa_NFW(x,y,gx,gy,kappa,rscale,ell,ell_pa):
         else:
             f = 1 - 2/sqrt(u*u-1) * arctan(sqrt((u-1)/(u+1)))
         f /= (u*u-1)
-    return kappa*f
+    return 2*kappa*f
 
     
--- systems/many.py
+++ systems/many.py
@@ -1,15 +1,14 @@
-from numpy import sqrt, pi, cos, sin, arctan2, arctan, arctanh
 from numpy import linspace, zeros, amin, amax
 from pylab import contour, figure, savefig, show
 from Laplace_function import Laplace
-from kappas import kappa_NFW
+from kappas import kappa_SIE, poten_shear, kappa_NFW
 
 import params
 sim = params.read()
 
 
 def geom(src,x,y):
-    z = zeros((len(x),len(y)))
+    z = zeros((len(y),len(x)))
     bx,by = float(src[1]), float(src[2])
     for i in range(len(x)):
         for j in range(len(y)):
@@ -17,45 +16,37 @@ def geom(src,x,y):
     return z
 
 
-def shear(xs,X,Y):  
-    z = zeros((len(Y),len(X)))
-    gam,thg = float(xs[1]), float(xs[2])*pi/180 + pi/2
-    for i in range(len(X)):
-        for j in range(len(Y)):
-            x,y = X[i],Y[j]
-            rsq = x*x + y*y
-            f = 2*(arctan2(y,x) - thg)
-            z[j,i] = -gam/2*rsq*cos(f)
+def shear(xs,x,y):  
+    z = zeros((len(y),len(x)))
+    sh,pa = float(xs[1]), float(xs[2])
+    for i in range(len(x)):
+        for j in range(len(y)):
+            z[j,i] = poten_shear(x[i],y[j],sh,pa)
     return z
 
 
     
 def SIE(gal,x,y):
-    z = zeros((len(x),len(y)))
-    re,ell = float(gal[1]), float(gal[2])
-    pa = float(gal[3])*pi/180 + pi/2
+    z = zeros((len(y),len(x)))
+    re,ell,ell_pa = float(gal[1]), float(gal[2]), float(gal[3])
     print 'Einstein radius',re
     for i in range(len(x)):
         for j in range(len(y)):
-            phi = arctan2(y[j],x[i]) - pa
-            th2r = x[i]**2 + y[j]**2 + 1e-6
-            th2p = 1 - ell*cos(2*phi)
-            theta = (th2r*th2p)**.5
-            z[j,i] = re/theta
+            z[j,i] = kappa_SIE(x[i],y[j],re,ell,ell_pa)
     print 'max kappa',amax(z)
     return z
 
-def NFW(gal,X,Y):
-    z = zeros((len(Y),len(X)))
+def NFW(gal,x,y):
+    z = zeros((len(y),len(x)))
     gx,gy = float(gal[1]), float(gal[2])
     kap,rsc = float(gal[3]), float(gal[4])
     ell,pa = float(gal[5]),float(gal[6])
     print 'pos',gx,gy
     print 'kap,rsc',kap,rsc
     print 'ell,pa',ell,pa
-    for i in range(len(X)):
-        for j in range(len(Y)):
-            z[j,i] = kappa_NFW(X[i],Y[j],gx,gy,kap,rsc,ell,pa)
+    for i in range(len(x)):
+        for j in range(len(y)):
+            z[j,i] = kappa_NFW(x[i],y[j],gx,gy,kap,rsc,ell,pa)
     print 'max kappa',amax(z)
     return z
 
@@ -66,16 +57,14 @@ def grids(asw,x,y):
     if obj[0]=='quasar' or obj[0]=='galaxy':
         gal = obj[2]
         kappa = SIE(gal,x,y)            
-        delta = x[1]-x[0]
-        arriv += Laplace(2*kappa*delta**2)
         arriv -= shear(obj[3],x,y)
     if obj[0]=='cluster':
         kappa = 0*arriv
         clus = obj[2]
         for gal in clus:
             kappa += NFW(gal,x,y)
-        delta = x[1]-x[0]
-        arriv += Laplace(2*kappa*delta**2)
+    delta = x[1]-x[0]
+    arriv += Laplace(2*kappa*delta**2)
     return (kappa, arriv)
     
 
@@ -90,7 +79,7 @@ def draw(asw):
     fig = figure()
     panel = fig.add_subplot(1,1,1)
     panel.set_aspect('equal')
-    lev = linspace(0,amax(kappa),10)
+    lev = linspace(0,amax(kappa),20)
     pc = panel.contour(x,y,kappa,lev)
     panel.clabel(pc, inline=1, fontsize=10)
     savefig(asw+'_kappa.png')
@@ -108,11 +97,4 @@ for asw in sim:
         draw(asw)
         break
 
-
-#kappa, arriv = grids('ASW0002b6m',x,y)
-#kappa, arriv = grids('ASW000102p',x,y)
-#kappa, arriv = grids('ASW00019rw',x,y)
-#kappa, arriv = grids('ASW0000r8n',x,y)
-#kappa, arriv = grids('ASW0001hpf',x,y)
-#kappa, arriv = grids('ASW0000h2m',x,y)
 

f19ecf1a3bda136439f593d833b6f0aa76e93427
RafiKueng <rafi.kueng@gmx.ch>
2013-09-08 21:16:17 +0200

    update tables, comment gen_tables

--- plots/gen_table.py
+++ plots/gen_table.py
@@ -15,11 +15,21 @@ import csv
 from fractions import Fraction as Frac
 
 
+#==============================================================================
+# SETTINGS
+#==============================================================================
+
 inp_file_name = 'overview.csv'
 inp_file_path = '../spaghetti/data/mod_chal'
 
 out_file_name = 'results.tex'
 out_file_path = '../text/tab/auto'
+
+# for tex template settings see below
+
+#==============================================================================
+# PARSING
+#==============================================================================
 
 inpfile = os.path.join(os.path.abspath(inp_file_path),inp_file_name)
 outfile = os.path.join(os.path.abspath(out_file_path),out_file_name)
@@ -36,6 +46,7 @@ with open(inpfile, 'r') as f:
   for i, row in enumerate(csvr):
     #print row
 
+    # get the stats from the end of the file
     if i in [156,159,162,165]:
       
       all_stats.append({
@@ -49,27 +60,31 @@ with open(inpfile, 'r') as f:
         'rightOrder' : int(row[14]),
         'err'        : [int(_) for _ in row[16:26]]
       })
-  
+
+    # skip if not in data rows  
     if i<2 or i>155 or (row[0]=='' and row[1]==''):
       continue
     
-    if row[0]!='':
+    
+    if row[0]!='': # if header row containing sim name
       #print row[0]
       simname=row[0]
       all_data[simname]=[]
       
-    else:
-      if simname=='ASW0001gae':
+    else: # regular data
+      if simname=='ASW0001gae': # we don't like this sim
         continue
-      #print row[1], row[29], row[30], row[31]
 
+      # recover the fractions in row 12 and 13 as such..
+      # makes sure strings like '2/3', '1 3/5' ect are recovered as math. fractions/numbers
       r12 = reduce(lambda x, y: x+y, [Frac(_) for _ in row[12].split(' ') if _!='']).limit_denominator()
       r13 = reduce(lambda x, y: x+y, [Frac(_) for _ in row[13].split(' ') if _!='']).limit_denominator()
 
-      #true / false values / strings
+      #true / false values / strings in output
       t = "X"
       f = 'O'
 
+      # read rows
       data = {
         'id'         : row[1],
         'npnt'       : t if row[ 7]=='x' else f,
@@ -85,26 +100,25 @@ with open(inpfile, 'r') as f:
         'fav2'       : True if row[31]=='1' else False,
       }
       
+      # parse the errors into a string of error number
       a = row[16:26]
-      #print a
-      a = [_=='x' for _ in a]
-      #print a
-      
-      s=[]
+      a = [_=='x' for _ in a] # list of bools
+      s=[] # list of strings with err id
       for j, l in enumerate(a):
         if l:
           s.append(str(j+1))
-          
-      er = ','.join(s)
+      er = ','.join(s) #final string ,-sep ids
       data['errors'] = er
       
-      #print data
       all_data[simname].append(data)
 
 
-texstr = ''
 
+#==============================================================================
+# LONG ALL INCL TABLE SETTINGS / TEMPLATE
+#==============================================================================
 
+# caption
 ca = '''1. col: places of imgs idendited correctly; 2. col: more or less
 correct identification of extr points. 3.col: exact identification of extremal
 points. 4.col: type(s) of errors ocured....
@@ -127,7 +141,6 @@ settings = {
 }
       
 
-
 #tex_head_tmpl = r'''
 #\begin{table}
 #  \centering
@@ -142,12 +155,13 @@ tex_head_tmpl = r'''
 ''' % settings
 
 
-
+# sim header row
 tex_rowh_tmpl = r'''
       \multicolumn{6}{l}{%s}\\
       \hline
 '''
 
+#regular row
 tex_row_tmpl  = r'''      & %(id)s & %(approxPlace)s & %(approx)s & %(veryRight)s & %(errors)s\\
 '''
 
@@ -170,10 +184,11 @@ tex_foot_tmpl = r'''
 ''' % settings
 
 
-############################################
+#==============================================================================
+# ONE TABLE PER SIM TABLESETTINGS / TEMPLATE
+#==============================================================================
 
 s_settings = settings
-
 
 s_tbl_head = r'''
 \begin{table}
@@ -189,54 +204,78 @@ s_tbl_foot = r'''
 '''
 
 
+#==============================================================================
+# THE TEX GENERATION
+#==============================================================================
 
+texstr = ''
 texstr += tex_head_tmpl
 for simname, data in all_data.items():
-  #print simname#, data
-  texstr += tex_rowh_tmpl % simname
-  s_tbl = s_tbl_head
+
+  texstr += tex_rowh_tmpl % simname   # create section header for long table
+  s_tbl = s_tbl_head # create table header for short table
+
+  # collect data
   for d in data:
-    if not d['use']:
+    if not d['use']: #skip if it's marked not to be used in spread sheet
       continue
-    #print d
-    #print tex_row_tmpl % d
-    texstr += tex_row_tmpl % d
-    s_tbl += tex_row_tmpl % d
+    
+    texstr += tex_row_tmpl % d #make tex string for long table
+    s_tbl += tex_row_tmpl % d  #make tex string for short table
+
+  # generate table footer for short table
   s_tbl += s_tbl_foot % {
     'capt': 'Bblablabla '+simname,
     'lbl' : simname,
   }
+  
+  #.. and save short table
   path = os.path.join(os.path.abspath(out_file_path), simname+'.tex')
   with open(path, 'w') as stex:
     stex.write(s_tbl)  
   
-
+# gen footer for lang table
 texstr += tex_foot_tmpl
-    
-    
+
+# ...and save it    
 with open(outfile, 'w') as tex:
   tex.write(texstr)
 
 
+#==============================================================================
+# THE CUMMULATED STATUS TABLE
+#==============================================================================
+
+# gen header
 tex_stats = r'\begin{table}\centering\begin{tabular}{lcc}\hline'
 tex_stats += '\n'
 tex_stats += r'value & n & p \\'
 tex_stats += '\n'
 tex_stats += r'\hline'
 tex_stats += '\n'
+
+# get number of samples (for % calc)
 nn=int(all_stats[1]['n'])
+
 for key, val in all_stats[1].items():
-  if key=='err':
+  if key=='err': # split up errors
     for m, v in enumerate(val):
       tex_stats += r' %s & %s & %.2f\\' % (key+'%2i'%m, v, float(v)/nn)
       tex_stats += '\n'
-  elif key=='':
+
+  elif key=='': # should not happend...
     pass
-  else:
+  
+  else: # normal case
     tex_stats += r' %s & %s & %.2f\\' % (key, val, float(val)/nn)
+
   tex_stats += '\n'
+
+# table footer
 tex_stats += '\n'
 tex_stats += r'\end{tabular}\caption{Stats}\label{tab:stats}\end{table}'
+
+# ... and save
 path = os.path.join(os.path.abspath(out_file_path), '_stats.tex')
 with open(path, 'w') as stex:
   stex.write(tex_stats)  

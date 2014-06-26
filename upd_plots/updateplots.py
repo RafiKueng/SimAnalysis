466e215f23cc8c0560cb097078374c9cef4eb583
RafiKueng <rafi.kueng@gmx.ch>
2014-06-26 15:31:32 +0200

    working on updating the plots

---
+++ upd_plots/updateplots.py
@@ -0,0 +1,7 @@
+glass_basis('glass.basis.pixels', solver=None)
+
+#g = loadstate(path+'state.txt')
+#g.make_ensemble_average()
+#g.srcdiff_plot_adv(g.ensemble_average, night=True, upsample=8)
+#pl.savefig(path+'img3_ipol.png', facecolor='black', edgecolor='none')
+#pl.close()

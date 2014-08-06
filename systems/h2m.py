4bbf1dd6c177aaea336fe5f823ec85c83d1a3a80
Prasenjit Saha <psaha@physik.uzh.ch>
2014-08-06 11:45:42 +0200

    whatever

--- systems/h2m.py
+++ systems/h2m.py
@@ -1,4 +1,4 @@
-from numpy import linspace, amin, amax, cos, arctan2
+from numpy import zeros, linspace, amin, amax, sqrt, pi, cos, sin, arctan, arctanh, arctan2
 from pylab import figure, show
 from many import grids
 
@@ -44,12 +44,31 @@ def alpha_SIE(x,y,reinst,ell,ell_pa):
 
 def alpha_shear(x,y,sh_str,sh_pa):
     thg = sh_pa*pi/180 + pi/2
-    rsq = x*x + y*y
+    r = sqrt(x*x + y*y)
     f = arctan2(y,x) - 2*thg
     return (-sh_str*r*cos(f),sh_str*r*sin(f))
 
-def disp(asw,x,y):
+def far(asw,x,y):
+    dx = zeros((len(y),len(x)))
+    dy = 1*dx
     obj = sim[asw]
+    src = obj[1]
+    bx,by = float(src[1]), float(src[2])
+    gal = obj[2]
+    reinst,ell,ell_pa = float(gal[1]), float(gal[2]), float(gal[3])
+    print reinst,ell,ell_pa
+    xs = obj[3]
+    sh_str,sh_pa = float(xs[1]), float(xs[2])
+    for i in range(len(x)):
+        for j in range(len(y)):
+            phix,phiy = alpha_SIE(x[i],y[j],reinst,ell,ell_pa)
+            shx,shy = alpha_shear(x[i],y[j],sh_str,sh_pa)
+            r = sqrt(x[i]**2+y[j]**2)
+            dx[j,i] = bx - x[i] + reinst*x[i]/r
+            dy[j,i] = by - y[j] + reinst*y[j]/r
+    return dx*dx + dy*dy
+
+
 
 sim = readq()
 
@@ -57,12 +76,15 @@ print sim['ASW0000h2m']
 N,R = 100,20
 x = linspace(-R,R,N)
 y = 1*x
-kappa,arriv = grids('ASW0000h2m',x,y)
+# kappa,arriv = grids('ASW0000h2m',x,y)
+lum = far('ASW0000h2m',x,y)
 fig = figure()
 panel = fig.add_subplot(1,1,1)
 panel.set_aspect('equal')
-lo,hi = amin(arriv), amax(arriv)
-lev = linspace(lo,lo+.2*(hi-lo),100)
-panel.contour(x,y,arriv,lev)
+# lo,hi = amin(lum), amax(lum)
+lev = linspace(-0.01,0.01,10)
+panel.contour(x,y,lum,lev)
+#panel.contour(x,y,lum[0],lev)
+#panel.contour(x,y,lum[1],lev)
 show()
 
